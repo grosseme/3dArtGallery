54c3f744090f67b4e03d4d4f8565b7926655dd61
ashlyn02 <ashlyn3090@gmail.com>
2024-07-16 13:54:13 -0400

    Update random_ray_vertices.py

--- visualization/src/random_ray_vertices.py
+++ visualization/src/random_ray_vertices.py
@@ -3,8 +3,8 @@ import numpy as np
 import polyscope as ps
 from src.read_skeleton import read_skeleton
 
-def rand_ray():
-  S_V = read_skeleton('spot')
+def rand_ray(filename):
+  S_V = read_skeleton(filename)
   
   num_vert = len(S_V)
   vecs= np.random.rand(num_vert, 3)
