54a517898bf251ea30c5d2318343d9ec0182362c
Mutiraj Laksanawisit <mutiraj@bu.edu>
2024-07-17 01:03:34 +0700

    Adding random rays to visualization_main

--- visualization/src/random_ray_vertices.py
+++ visualization/src/random_ray_vertices.py
@@ -3,14 +3,8 @@ import numpy as np
 import polyscope as ps
 from src.read_skeleton import read_skeleton
 
-def rand_ray():
-  S_V = read_skeleton('spot')
-  
+def rand_ray(S_V):
   num_vert = len(S_V)
-  vecs= np.random.rand(num_vert, 3)
+  vecs= np.random.rand(num_vert, 3) - 0.5
   
-return vecs
-  
-
-
-
+  return vecs
--- visualization/visualization_main.py
+++ visualization/visualization_main.py
@@ -4,9 +4,11 @@ from src.read_mesh import read_mesh
 from src.read_skeleton import read_skeleton
 from src.display_mesh import display_mesh
 from src.skeleton_points_sampling import skeleton_points_fixed_length_sampling
+from src.random_ray_vertices import rand_ray
 
-V, F = read_mesh('homer')
-S_V, S_E = read_skeleton('homer')
-S_P = skeleton_points_fixed_length_sampling(S_V, S_E, 1e-2)
+V, F = read_mesh('spot')
+S_V, S_E = read_skeleton('spot')
+S_P = skeleton_points_fixed_length_sampling(S_V, S_E, 5e-3)
+vecs = rand_ray(S_V)
 
-display_mesh(V, F, S_V = S_V, S_E = S_E, S_P = S_P)
+display_mesh(V, F, S_V = S_V, S_E = S_E, S_P = S_P, vecs = vecs)
