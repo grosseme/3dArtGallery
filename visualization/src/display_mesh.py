4d0bca9936c7c0dc72faae40516f2dc179babd76
Mutiraj Laksanawisit <mutiraj@bu.edu>
2024-07-17 01:23:50 +0700

    visualization_main now accept filename as input

--- visualization/src/display_mesh.py
+++ visualization/src/display_mesh.py
@@ -8,21 +8,21 @@ def display_mesh(V, F, R_V = [], R_E = [], intersected = [], C_V = [], S_V = [],
     obj.set_transparency(0.6)
 
     if len(intersected) != 0:
-        obj.add_scalar_quantity('Intersected', intersected, defined_on='faces', cmap='reds')
+        obj.add_scalar_quantity('Intersected', intersected, defined_on='faces', cmap='reds', enabled = True)
 
     if len(C_V) != 0:
         ps.register_point_cloud('Intersection', C_V, radius=0.002)
 
     if len(R_V) != 0 and len(R_E) != 0:
-        ps.register_curve_network('THE RAY', R_V, R_E)
+        ps.register_curve_network('THE RAY', R_V, R_E, radius = 0.002)
 
     if len(S_V) != 0 and len(S_E) != 0:
-        skeleton = ps.register_curve_network('Skeleton', S_V, S_E, radius=0.001)
+        skeleton = ps.register_curve_network('Skeleton', S_V, S_E, radius = 0.001)
 
         if len(S_P) != 0:
-            ps.register_point_cloud('Skeleton Points Sampling', S_P, radius=0.002)
+            ps.register_point_cloud('Skeleton Points Sampling', S_P, radius = 0.002)
 
         if len(vecs) != 0:
-            skeleton.add_vector_quantity("Random ray vector", vecs, enabled=True, length = 1)
+            skeleton.add_vector_quantity("Random ray vector", vecs, enabled=True, radius = 0.002, length = 1)
 
     ps.show()
--- visualization/visualization_main.py
+++ visualization/visualization_main.py
@@ -6,8 +6,9 @@ from src.display_mesh import display_mesh
 from src.skeleton_points_sampling import skeleton_points_fixed_length_sampling
 from src.random_ray_vertices import rand_ray
 
-V, F = read_mesh('spot')
-S_V, S_E = read_skeleton('spot')
+filename = input()
+V, F = read_mesh(filename)
+S_V, S_E = read_skeleton(filename)
 S_P = skeleton_points_fixed_length_sampling(S_V, S_E, 5e-3)
 vecs = rand_ray(S_V)
 
